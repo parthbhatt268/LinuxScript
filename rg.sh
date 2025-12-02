d09f5835f43da4e65540efdde3409430f722350e
parthbhatt268 <parth.bhatt@siren.io>
2025-12-02 14:39:35 +0000

    script

--- rg.sh
+++ rg.sh
@@ -3,3 +3,5 @@ pkill -x chrome
 sleep 1
 google-chrome --restore-last-session >/dev/null 2>&1 &
 
+# I need this script because sometimes the cursor size becomes huge after some time,
+# so just to refresh the chrome session, we can simply run this script it close all tab and reopen them again.
